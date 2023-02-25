185cafe7d76611b839bd942cc5c0020d977df095
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-02-25 00:30:10 -0300

    ex3 ok

--- SchoolStudents/src/Application/Program.java
+++ SchoolStudents/src/Application/Program.java
@@ -3,6 +3,8 @@ package Application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import Entities.Student;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -10,6 +12,20 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		Student student = new Student();
+		System.out.print("Student's name: ");
+		student.name = sc.nextLine();
+		System.out.print("Student's nota1: ");
+		student.nota1 = sc.nextDouble();
+		System.out.print("Student's nota2: ");
+		student.nota2 = sc.nextDouble();
+		System.out.print("Student's nota3: ");
+		student.nota3 = sc.nextDouble();
+		
+		System.out.printf("FINAL GRADE: %.2f%n", student.media());
+		student.result();
+		
+		sc.close();
 	}
 
 }
--- SchoolStudents/src/Entities/Student.java
+++ SchoolStudents/src/Entities/Student.java
@@ -4,5 +4,19 @@ public class Student {
 	
 	public String name;
 	public double nota1, nota2, nota3;
+	
+	public double media() {
+		return (nota1 + nota2 + nota3) ;
+	}
+	
+	public void result () {
+		if (media() >= 60.00) {
+			System.out.println("PASS");
+		} else {
+			System.out.println("FAILED");
+			System.out.printf("MISSING %.2f POINTS", (60.00 - media()));
+		}
+	}
+	
+
 }
-s
