6b6f1c85df68ea02b377a728d68d89e2be4c3bb0
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-02-26 18:34:48 -0300

    final ex

--- ExStaticClass/src/Application/Program.java
+++ ExStaticClass/src/Application/Program.java
@@ -1,10 +1,27 @@
 package Application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import Entities.CurrencyConverter;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("What is the dollar price? ");
+		double price = sc.nextDouble();
+		System.out.print("How many dollars will you be bought? ");
+		double quantity = sc.nextDouble();
+		
+		double reais = CurrencyConverter.conversao(price, quantity);
+		
+		System.out.printf("Amout to be paid in reais = %.2f%n", reais);
+		
+		sc.close();
 	}
 
 }
--- ExStaticClass/src/Entities/CurrencyConverter.java
+++ ExStaticClass/src/Entities/CurrencyConverter.java
@@ -1,5 +1,12 @@
 package Entities;
 
 public class CurrencyConverter {
+	
+	public static final double TAX = 0.06;
+	
+	public static double conversao (double price, double quantity) {
+		return (price * (quantity + quantity * TAX));
+	}
+	
 
 }
