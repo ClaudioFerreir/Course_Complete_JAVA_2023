cc20cdbcb81e7f7fc39c4632ee159c0521e15d75
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-02-28 12:13:15 -0300

    entity fila struture

---
+++ Fila/src/Entities/Fila.java
@@ -0,0 +1,39 @@
+package Entities;
+
+public class Fila {
+	
+	int tamanho, inicio, fim, total;
+	int vetor[];
+	
+	public Fila(int tamanho) {
+		inicio = 0;
+		fim = 0;
+		total = 0;
+		this.tamanho = tamanho;
+		vetor = new int[tamanho]; 
+	}
+	
+	public boolean FilaVazia() {
+		
+	}
+
+	public boolean FilaCheia() {
+		
+	}
+	
+	public void Enfileirar (int elemento) {
+		
+	}
+	
+	public int Desenfileirar() {
+		
+	}
+	
+	public void ElementoInicio() {
+		
+	}
+	
+	public void MostrarFila() {
+		
+	}
+}
