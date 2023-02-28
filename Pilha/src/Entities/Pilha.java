24d50e106ceea2a97342864b5f117a589df33341
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-02-27 23:15:00 -0300

    empilhar function

--- Pilha/src/Entities/Pilha.java
+++ Pilha/src/Entities/Pilha.java
@@ -23,11 +23,23 @@ public class Pilha {
 	}
 
 	public boolean PilhaCheia() {
+		
+	    if (topo >= tamanho-1) {
+          return true;
+	    } else {
+	      return false;
+	    }
 
 	}
 
 	public void Empilhar(int elemento) {
-
+		
+		  if (! PilhaCheia( )) {
+		      topo = topo + 1;
+		      vetor[topo] = elemento;
+		  } else {
+		      System.out.println ("Pilha Cheia");
+		    }
 	}
 
 	public void Desempilhar() {
