33585f0e2d9259c671b0f590bb6ace3ccbb59b8d
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-02-28 13:18:22 -0300

    program ok

--- Fila/src/Entities/Fila.java
+++ Fila/src/Entities/Fila.java
@@ -1,5 +1,7 @@
 package Entities;
 
+import javax.swing.JOptionPane;
+
 public class Fila {
 	
 	int tamanho, inicio, fim, total;
@@ -15,25 +17,76 @@ public class Fila {
 	
 	public boolean FilaVazia() {
 		
+		if (total == 0) {
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 
 	public boolean FilaCheia() {
+		
+		if (total >= tamanho) {
+			return true;
+		} else {
+			return false;
+		}
 		
 	}
 	
 	public void Enfileirar (int elemento) {
 		
+		if (FilaCheia()) {
+			System.out.print("Fila Cheia");
+		} else {
+			vetor[fim] = elemento;
+			fim = fim + 1;
+			total = total + 1;
+			if (fim >= tamanho) {
+				fim = 0;
+			}
+		}
+		
 	}
 	
 	public int Desenfileirar() {
 		
+		int desenfileirado = 0;
+		if (FilaVazia()) {
+			System.out.println("Fila Vazia");
+		} else {
+			desenfileirado = vetor[inicio];
+			inicio = inicio + 1;
+			total = total - 1;
+			if (inicio >= tamanho) {
+				inicio = 0;
+			}
+		}
+		return desenfileirado;
 	}
 	
 	public void ElementoInicio() {
+		
+		if (FilaVazia()) {
+			System.out.println("Fila Vazia");
+		} else {
+			System.out.println("O primeiro elemento é " + vetor[inicio]);
+		}
 		
 	}
 	
 	public void MostrarFila() {
 		
+		int i, aux;
+		
+		aux = inicio;
+		for (i = 1; i <= total; i++) {
+			JOptionPane.showInputDialog(null, "Elemento " + vetor[aux] + " posição " + i);
+			aux = aux + 1;
+			if (aux >= tamanho) {
+				aux = 0;
+			}
+		}
 	}
 }
