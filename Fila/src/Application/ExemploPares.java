16bdda0e28ca631274ff2c6a824a963af3abf01e
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-02-28 12:21:00 -0300

    main program ok

---
+++ Fila/src/Application/ExemploPares.java
@@ -0,0 +1,26 @@
+package Application;
+
+import javax.swing.JOptionPane;
+
+import Entities.Fila;
+
+public class ExemploPares {
+
+	public static void main(String[] args) {
+		
+		Fila intFila = new Fila(10);
+		int i, entrada = 0;
+		
+		for (i = 1; i <= 10; i ++) {
+			entrada = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro"));
+		
+			if (entrada % 2 == 0) {
+				intFila.Enfileirar(entrada);
+			}
+		}
+		intFila.MostrarFila();
+		System.exit(0);
+
+	}
+
+}
--- Fila/src/module-info.java
+++ Fila/src/module-info.java
@@ -6,4 +6,5 @@
  *
  */
 module Fila {
+	requires java.desktop;
 }
