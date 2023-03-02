e9e0b4cac5fa6ef89f8af6c91ea5da3ca84fc139
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-01 21:10:18 -0300

    arvore busca struture

--- ArvoreBinariaBusca/src/entities/ArvoreBinaria.java
+++ ArvoreBinariaBusca/src/entities/ArvoreBinaria.java
@@ -1,5 +1,42 @@
 package entities;
 
 public class ArvoreBinaria {
-
+	
+	private BIntNo Raiz;
+	
+	private BIntNo inserir (BIntNo arvore, int novoNo) {
+		
+	}
+	
+	public void inserirNo (int novoValor) {
+		
+	}
+	
+	private void exibirEsquerdo (BIntNo arv) {
+		
+	}
+	
+	private void exibirDireito (BIntNo arv) {
+		
+	}
+	
+	public void exibirRaiz() {
+		
+	}
+	
+	public void exibirNoEsq() {
+		
+	}
+	
+	public void exibirNoDir() {
+		
+	}
+	
+	public void exibirNo() {
+		
+	}
+	
+	public void excluirNo(int item) {
+		
+	}
 }
