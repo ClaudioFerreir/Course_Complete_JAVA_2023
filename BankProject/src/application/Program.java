6e8c59195c8c4292cc68d4298dee1ea038bc8b22
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-06 12:04:23 -0300

    first part ok

--- BankProject/src/application/Program.java
+++ BankProject/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Locale;
 import javax.swing.JOptionPane;
+import entities.Conta;
 
 public class Program {
 
@@ -9,18 +10,19 @@ public class Program {
 	
 		Locale.setDefault(Locale.US);
 		
-		double conta =  Double.parseDouble(JOptionPane.showInputDialog("Enter your account number: "));
+		int numero =  Integer.parseInt(JOptionPane.showInputDialog("Enter your account number: "));
 		String titular = JOptionPane.showInputDialog("Enter account holder: ");
 		String option = "";
 		double valor;
  		
 		do {
 			option = JOptionPane.showInputDialog("Is there na initial deposit (y/n)?");
-		} while (option == "y") {
 			valor =  Double.parseDouble(JOptionPane.showInputDialog("Enter initial deposit value: "));
-		}
+		} while (option == "y");
 		
-		
+		Conta conta = new Conta(numero, titular, valor);
+		System.out.println("Account data: ");
+		System.out.println("Account " + numero + ", Holder: " + titular + ", Balance: $ " + valor);
 
 	}
 
