d19025a9207b8789b9de5bb2625af7b08e897722
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-06 11:20:17 -0300

    getters and setters

--- BankProject/src/entities/Conta.java
+++ BankProject/src/entities/Conta.java
@@ -1,5 +1,30 @@
 package entities;
 
 public class Conta {
+	
+	private int conta;
+	private String titular;
+	private double valor;
+	
+	// constructor
+	public Conta (int conta, String titular, double valor) {
+		this.conta = conta;
+		this.titular = titular;
+		this.valor = valor;
+	}
+	
+	public Conta (int conta, String titular) {
+		this.conta = conta;
+		this.titular = titular;
+	}
+	
+	//getters and setters
+	public int getConta() {
+		return conta; 
+	}
+	
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
 
 }
