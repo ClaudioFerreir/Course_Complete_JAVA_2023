a3a49def37cff6868ca6dbddd2a1bec0efb8d7f5
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-06 14:50:08 -0300

    methods ok

--- BankProject/src/application/Program.java
+++ BankProject/src/application/Program.java
@@ -12,18 +12,54 @@ public class Program {
 		
 		int numero =  Integer.parseInt(JOptionPane.showInputDialog("Enter your account number: "));
 		String titular = JOptionPane.showInputDialog("Enter account holder: ");
-		String option = "";
-		double valor;
+		double valor = 0;
  		
-		do {
-			option = JOptionPane.showInputDialog("Is there na initial deposit (y/n)?");
+		String option = JOptionPane.showInputDialog("Is there na initial deposit (y/n)?");
+		if (option == "y") {	
 			valor =  Double.parseDouble(JOptionPane.showInputDialog("Enter initial deposit value: "));
-		} while (option == "y");
+		}; 
 		
 		Conta conta = new Conta(numero, titular, valor);
+		
 		System.out.println("Account data: ");
-		System.out.println("Account " + numero + ", Holder: " + titular + ", Balance: $ " + valor);
-
+		System.out.println("Account " + conta.getConta() + ", Holder: " + conta.getTitular() + ", Balance: $ " + conta.getValor());
+		
+		
+		
+		
 	}
+		
 
+		
+		public static void accountOP(Conta conta) {
+			
+			String option2 = JOptionPane.showInputDialog("Would you like to do any operation (y/n)?");
+			if (option2 == "y") {
+				System.out.println("COD-------OPERATION");
+				System.out.println("-------------------");
+				System.out.println(" 1          Deposit");
+				System.out.println(" 2         Withdraw");
+				
+				int option3 =  Integer.parseInt(JOptionPane.showInputDialog("Enter operation code: "));
+				int valor = 0;
+				
+				switch(option3) {
+				case 1:
+					conta.depositar(valor);
+					break;
+				case 2:
+					conta.saque(valor);
+					break;
+				default:
+					System.out.println("Invalid Code");
+					break;
+				}
+				System.out.println("Udapted account data: ");
+				System.out.println("Account " + conta.getConta() + ", Holder: " + conta.getTitular() + ", Balance: $ " + conta.getValor());
+			}
+
+		}
+	
 }
+
+
