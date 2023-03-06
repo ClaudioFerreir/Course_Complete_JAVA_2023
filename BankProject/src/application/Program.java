ddec61b0410150f1ddb337082caeb4c59dbbd325
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-06 11:50:25 -0300

    initial variables

--- BankProject/src/application/Program.java
+++ BankProject/src/application/Program.java
@@ -1,9 +1,26 @@
 package application;
 
+import java.util.Locale;
+import javax.swing.JOptionPane;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Locale.setDefault(Locale.US);
+		
+		double conta =  Double.parseDouble(JOptionPane.showInputDialog("Enter your account number: "));
+		String titular = JOptionPane.showInputDialog("Enter account holder: ");
+		String option = "";
+		double valor;
+ 		
+		do {
+			option = JOptionPane.showInputDialog("Is there na initial deposit (y/n)?");
+		} while (option == "y") {
+			valor =  Double.parseDouble(JOptionPane.showInputDialog("Enter initial deposit value: "));
+		}
+		
+		
 
 	}
 
