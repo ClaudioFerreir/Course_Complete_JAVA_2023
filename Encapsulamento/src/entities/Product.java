1da19466db4c02cbc3e20274a7c31de39ed5714c
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-06 10:55:01 -0300

    bank project

--- Encapsulamento/src/entities/Product.java
+++ Encapsulamento/src/entities/Product.java
@@ -10,40 +10,38 @@ public class Product {
 	public Product() {
 	}
 	
-	public Product (String name, double price, int quantity) {
+	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
 	}
-	
-	// sobrecarga
-	public Product (String name, double price) {
+
+	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;
 	}
 	
-	//getters e setters
-	public void setName(String name) {
-		this.name = name;
-	}
-	
+	//getters and setters
 	public String getName() {
 		return name;
 	}
-	
-	public void setPrice (double price) {
-		this.price = price;
+
+	public void setName(String name) {
+		this.name = name;
 	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
-	
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 	public int getQuantity() {
 		return quantity;
 	}
-	
+
 	//funcoes
 	public double totalValueInStock() {
 		return price * quantity;
