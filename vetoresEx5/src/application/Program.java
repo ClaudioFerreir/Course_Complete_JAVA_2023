4c1704910f6c321c68c4d7a6142a47113e79735b
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-09 20:40:17 -0300

    ex6 ok

--- vetoresEx5/src/application/Program.java
+++ vetoresEx5/src/application/Program.java
@@ -1,10 +1,36 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		System.out.print("Quantos numeros voce vai digitar? ");
+		n = sc.nextInt();
+		
+		double[] vetor = new double[n];
+		
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print("Digite um numero: ");
+			vetor[i] = sc.nextDouble();
+		}
+		
+		double maior = 0;
+		int posicao = 0;
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i] > maior) {
+				maior = vetor[i];
+				posicao = i;
+			}
+		}
+		System.out.printf("MAIOR VALOR = %.1f%n", maior);
+		System.out.print("POSICAO DO MAIOR VALOR = " + posicao);
 	}
 
 }
