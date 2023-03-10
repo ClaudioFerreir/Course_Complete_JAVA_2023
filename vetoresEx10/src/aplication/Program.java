b5cc98d03b952f4ac5ffca21ff1e6874371b7975
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-10 09:22:22 -0300

    ex10 ok

---
+++ vetoresEx10/src/aplication/Program.java
@@ -0,0 +1,37 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		System.out.print("Quantos alunos serao digitados? ");
+		n = sc.nextInt();
+		
+		String[] vetorNomes = new String[n];
+		double[] vetorNota1 = new double[n];
+		double[] vetorNota2 = new double[n];
+		
+		for (int i = 0; i < vetorNomes.length; i++) {
+			System.out.println("Digite nome, primeira e segunda nota do " + (i+1) + "o aluno: ");
+			vetorNomes[i] = sc.next();
+			vetorNota1[i] = sc.nextDouble();
+			vetorNota2[i] = sc.nextDouble();
+		}
+		
+		System.out.println("Alunos aprovados: ");
+		for (int i = 0; i < vetorNomes.length; i++) {
+			if (((vetorNota1[i] + vetorNota2[i])/2) >= 6.0) {
+				System.out.println(vetorNomes[i]);
+			}
+		}
+		sc.close();
+	}
+
+}
