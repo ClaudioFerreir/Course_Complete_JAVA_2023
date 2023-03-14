55649c845363dd0dd501fddcb5e0852eb85d010c
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-14 18:37:56 -0300

    Array list methods

--- lists/src/lists/List.java
+++ lists/src/lists/List.java
@@ -1,6 +1,8 @@
 package lists;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class List {
 
@@ -13,11 +15,34 @@ public class List {
 		list.add("Dulce");
 		list.add("Galrao");
 		list.add("Robgol");
+		list.add(2, "Marco");
+		
+	
+		System.out.println(list.size());
+		for (String x : list) {
+			System.out.println(x);
+		}
+		
+		System.out.println("------------------------");
+		//list.remove("Dulce");
+		//list.remove(3);
+		//list.removeIf(x -> x.charAt(0) == 'M');
 		
 		for (String x : list) {
 			System.out.println(x);
 		}
-
+		System.out.println("----------------------------------");
+		System.out.println("Index of Galrao: " + list.indexOf("Galrao"));
+		System.out.println("Index of Joao: " + list.indexOf("Joao"));
+		System.out.println("----------------------------------");
+		
+		ArrayList<String> result = (ArrayList<String>) list.stream().filter(x -> x.charAt(0) == 'M').collect(Collectors.toList());
+		for (String x : result) {
+			System.out.println(x);
+		}
+		System.out.println("----------------------------------");
+		String name = list.stream().filter(x -> x.charAt(0) == 'X').findFirst().orElse(null);
+		System.out.println(name);
 	}
 
 }
