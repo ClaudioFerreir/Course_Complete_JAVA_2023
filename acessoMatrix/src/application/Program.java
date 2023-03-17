894c80bd0f384414f8cb258267ce411a9fc575a3
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-17 18:57:44 -0300

    matrix ex

---
+++ acessoMatrix/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um numero para linhas:");
+		int l = sc.nextInt();
+		System.out.print("Digite um numero para colunas:");
+		int c = sc.nextInt();
+		
+		int[][] mat = new int[l][c];
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println(mat);
+		
+		sc.close();
+	}
+
+}
