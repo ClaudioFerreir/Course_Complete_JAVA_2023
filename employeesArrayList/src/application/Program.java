4dc0a7d1b9051f125d49f46a093d0e57a5d9ae4e
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-17 17:55:34 -0300

    matrix struture

--- employeesArrayList/src/application/Program.java
+++ employeesArrayList/src/application/Program.java
@@ -1,10 +1,8 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import entities.Employee;
 
