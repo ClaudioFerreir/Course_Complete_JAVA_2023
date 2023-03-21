94a3441d95b692c2df1119d3651491308819de6f
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-21 11:03:35 -0300

    time date string

--- data_hora_string/src/application/Program.java
+++ data_hora_string/src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class Program {
 
@@ -12,7 +14,24 @@ public class Program {
 		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
 		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 		
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		//formata a data hora para o fuso local e devemos especificar o fuso no caso de instant
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+		DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
 		
+		System.out.println("d04 = " + d04.format(fmt1));
+		System.out.println("d04 = " + fmt1.format(d04));
+		System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		System.out.println("d05 = " + d05.format(fmt1));
+		System.out.println("d05 = " + d05.format(fmt2));
+		System.out.println("d05 = " + d05.format(fmt4));
+		
+		System.out.println("d06 = " + fmt3.format(d06));
+		System.out.println("d06 = " + fmt5.format(d06));
+		System.out.println("d06 = " + d06.toString());
 	}
 
 }
