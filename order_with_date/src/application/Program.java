fc1c172c4d7705722965c337a529617d8eebf1c1
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-25 18:19:30 -0300

    order class

--- order_with_date/src/application/Program.java
+++ order_with_date/src/application/Program.java
@@ -1,14 +1,12 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
-import entities.OrderItem;
-
 public class Program {
 
 	public static void main(String[] args) {
 		
-		List<OrderItem> orderItem = new ArrayList<>();
+
+		
+		
 
 	}
 
---
+++ order_with_date/src/entities/Order.java
@@ -0,0 +1,49 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+	
+	Date moment;
+	OrderStatus status;
+	
+	List<OrderItem> orderItem = new ArrayList<>();
+	
+	public Order() {
+	}
+
+	public Order(Date moment, OrderStatus status) {
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public List<OrderItem> getOrderItem() {
+		return orderItem;
+	}
+
+	public void setOrderItem(List<OrderItem> orderItem) {
+		this.orderItem = orderItem;
+	}
+	
+	
+	
+	
+}
