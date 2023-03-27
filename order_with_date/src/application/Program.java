e1066df1ea89323da155ab939fcd74550ada6bd9
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-26 22:44:42 -0300

    output program

--- order_with_date/src/application/Program.java
+++ order_with_date/src/application/Program.java
@@ -1,10 +1,26 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
+		String name, email;
+		Date date;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter cliente data:");
+		System.out.print("Name: ");
+		sc.nextLine();
+		name = sc.nextLine();
+		System.out.print("Email: ");
+		email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		date = sc.nextLine();
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
 		
 		
 
