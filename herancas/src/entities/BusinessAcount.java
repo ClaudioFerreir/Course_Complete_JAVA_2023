07d11151de32707b87a671b42bf01eda03e87485
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-03-28 20:31:57 -0300

    businessAccount class added

--- herancas/src/entities/BusinessAcount.java
+++ herancas/src/entities/BusinessAcount.java
@@ -6,8 +6,26 @@ public class BusinessAcount extends Account {
 	
 	public BusinessAcount() {
 	}
+
+	public BusinessAcount(Double loanLimit) {
+		super();
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
 	
-	
+	public void loan(Double amount) {
+		if (amount <= this.loanLimit) {
+			this.loanLimit -= amount;
+			this.balance += amount;
+		}
+	}
 
 }
 	
