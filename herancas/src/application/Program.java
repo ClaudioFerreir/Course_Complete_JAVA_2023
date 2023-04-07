a9728c2949e257a15ccc45713cf101ef7a2ada42
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-04-06 23:25:51 -0300

    program renamed Business Account

--- herancas/src/application/Program.java
+++ herancas/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.Locale;
 
 import entities.Account;
+import entities.BusinessAccount;
 import entities.BusinessAcount;
 import entities.SavingsAccount;
 
@@ -14,12 +15,15 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		
 		Account acc = new Account(1001, "Alex", 0.0);
-		BusinessAcount bacc = new BusinessAcount(1002, "Maria", 0.0, 500.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 		
 		//UPCASTING
 		Account acc1 = bacc;
-		Account acc2 = new BusinessAcount(1003, "Bob", 0.0, 200.0);
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
 		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		
+		//DOWNCASTING
+		BusinessAccount acc4 = (BusinessAccount)acc2;
 	}
 
 }
